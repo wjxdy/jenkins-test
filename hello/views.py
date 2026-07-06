c18e33edbf3ef49724217832d46e561d2b71f471
特仑苏 <2077152374@qq.com>
2026-07-06 23:13:14 +0800

    feat: 测试一下功能

--- hello/views.py
+++ hello/views.py
@@ -2,8 +2,7 @@ from django.http import JsonResponse
 
 
 def hello(request):
-    return JsonResponse({'message': 'Hello from Django + Jenkins'})
-
+    return JsonResponse({'message': 'Hello from Django + Jenkins i love you'})
 
 def health(request):
     return JsonResponse({'status': 'ok'})
